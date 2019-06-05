793d3987322731588a349469a24acc791a82816a
gustavo <gustavoguguqueiroz@hotmail.com>
2019-06-05 11:41:23 -0300

    05 06 1

--- teste.c
+++ teste.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-int maior_2 (int a, int b) {
-  return (a > b) ? a : b;
-}
-int menor_2 (int a, int b) {
-  return (a < b) ? a : b;
-}
+
 int main () {
-  int a, b, c, aux_a, aux_b, aux_c;
-  scanf("%d %d %d", &a, &b, &c);
-  aux_a = maior_2 (a, maior_2 (b, c));
-  aux_c = menor_2 (a, menor_2 (b, c));
-  aux_b = a+b+c-aux_a-aux_c;
-  printf("%d %d %d\n", aux_a, aux_b, aux_c);
+  int a,b;
+  scanf("%d", &a);
+  for (a, b=a; a>=0; b+=a){
+    scanf("%d", &a);
+  }
+  printf("%d\n", b);
   return 0;
 }

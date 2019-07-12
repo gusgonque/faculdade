9d8c121c84a60ca26b8ef4abd5d43d32a7ad2685
Gustavo <gustavoguguqueiroz@hotmail.com>
2019-07-12 00:16:53 -0300

    12/07/2019 00:16

--- teste.c
+++ teste.c
@@ -1,26 +1,10 @@
 #include <stdio.h>
-#include <math.h>
-
-void encriptografia_numero_letra (int x) {
-  int aux_x = x, contador_divisoes = 0, aux_divisao, aux_resto;
-
-  for (contador_divisoes, aux_x; aux_x > 0; aux_x / 10) {
-    contador_divisoes ++;
-  }// dividindo x para saber qual o numero de posicoes.
-
-  for (aux_divisao = pow (10, contador_divisoes), aux_x = x, contador_divisoes, aux_resto = x; contador_divisoes >= 0; contador_divisoes --) {
-    aux_x /= aux_divisao;
-    printf("%c", (aux_x + 65));
-    aux_x = aux_resto % aux_divisao;
-  }
-  printf("\n");
-}
 
 int main () {
-  int num;
+  int num,i;
   scanf ("%d", &num);
-  for (num; num >= 0; scanf ("%d", &num)) {
-    encriptografia_numero_letra(num);
+  for (i = 0; i < num; i++) {
+    printf ("%d\n", i);
   }
   return 0;
 }

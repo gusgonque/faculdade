e05f80d706c9c93d6d51cc9f70a23b1e6d45be3e
Gustavo <gustavoguguqueiroz@hotmail.com>
2019-07-17 12:29:23 -0300

    17/07/2019 12:29

--- URI/C/1562.c
+++ URI/C/1562.c
@@ -2,20 +2,22 @@
 
 void pares ( int x ) {
   if ( x % 2 != 0) {
-    printf("IMPOSSIBLE2\n");
+    printf("IMPOSSIBLE\n");
   } else {
-    int alunos_sem_par[x], alunos_com_par[x], aux, aux_2;
+    int alunos_sem_par[2*x], alunos_com_par[2*x], aux;
     for ( aux = 1 ; aux <= x ; aux++) {
       scanf("%d", &alunos_sem_par[aux]);
       alunos_com_par[aux] = 0;
-      printf("O aluno %d quer ser par do aluno %d\n", aux, alunos_sem_par[aux]);
     }
-    for ( aux = 1 ; aux <= x ; aux++) {
-      if (alunos_com_par[alunos_sem_par[aux]] == 0) {
-        alunos_com_par[aux] = alunos_sem_par[aux];
-        alunos_com_par[alunos_sem_par[aux]] = aux;
-        printf("O par do aluno %d eh %d\n", aux, alunos_com_par[aux]);
+    for ( aux = 1 ; aux <= x ; aux++ ) {
+      if (alunos_com_par[aux] == 0) {
+        if (alunos_com_par[alunos_sem_par[aux]]==0) {
+          alunos_com_par[aux] = alunos_sem_par[aux];
+          alunos_com_par[alunos_sem_par[aux]] = aux;
+        }
+        else {}
       }
+      else {}
     }
     for ( aux = 1 ; aux <= x ; aux++) {
       printf("%d ", alunos_com_par[aux]);
@@ -28,7 +30,6 @@ int main () {
   int num_alunos_em_sala;
   scanf("%d", &num_alunos_em_sala);
   for (  ;  ; scanf("%d", &num_alunos_em_sala) ) {
-    printf("Numero de alunos = %d\n", num_alunos_em_sala);
     pares (num_alunos_em_sala);
   }
   return 0;
--- teste.c
+++ teste.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 
+void pares ( int x ) {
+  if ( x % 2 != 0) {
+    printf("IMPOSSIBLE2\n");
+  } else {
+    int alunos_sem_par[x], alunos_com_par[x], aux, aux_2;
+    for ( aux = 1 ; aux <= x ; aux++) {
+      scanf("%d", &alunos_sem_par[aux]);
+      alunos_com_par[aux] = 0;
+      printf("O aluno %d quer ser par do aluno %d\n", aux, alunos_sem_par[aux]);
+    }
+    for ( aux = 1 ; aux <= x ; aux++) {
+      if (alunos_com_par[alunos_sem_par[aux]] == 0) {
+        alunos_com_par[aux] = alunos_sem_par[aux];
+        alunos_com_par[alunos_sem_par[aux]] = aux;
+        printf("O par do aluno %d eh %d\n", aux, alunos_com_par[aux]);
+      }
+    }
+    for ( aux = 1 ; aux <= x ; aux++) {
+      printf("%d ", alunos_com_par[aux]);
+    }
+    printf("\n");
+  }
+}
+
 int main () {
-  int num,i;
-  scanf ("%d", &num);
-  for (i = 0; i < num; i++) {
-    printf ("%d\n", i);
+  int num_alunos_em_sala;
+  scanf("%d", &num_alunos_em_sala);
+  for (  ;  ; scanf("%d", &num_alunos_em_sala) ) {
+    printf("Numero de alunos = %d\n", num_alunos_em_sala);
+    pares (num_alunos_em_sala);
   }
   return 0;
 }

dc60cf35770d6ef3275901a032133ab3c05d7e87
Gustavo <gustavoguguqueiroz@hotmail.com>
2019-08-06 12:59:55 -0300

    06/08/2019 0.1

--- Computacao/Habib/Lista_01_08_2019_VETOR/23_Unfinished.c
+++ Computacao/Habib/Lista_01_08_2019_VETOR/23_Unfinished.c
@@ -5,6 +5,7 @@ void Identificar_Vetor (int v[], int n) {
   int aux;
   for (aux = 0; aux < n; aux ++) {
     scanf("%d", &v[aux]);
+    printf("v[%d]=%d\n", aux, v[aux]);
   }
 }
 
@@ -17,17 +18,19 @@ void Mostrar_Vetor (int v[], int n) {
 
 int Vetor_Soma_Subindo (int v[], int n, int Indice) {
   int aux, soma;
-  for ( aux = n, soma = 0 ; aux > n-Indice-1 ; aux-- ) {
-    soma += v[aux];
+  for ( aux = Indice, soma = 0 ; aux > 0 ; aux-- ) {
+    soma += v[aux-1];
   }
+  printf("Soma Subindo na posicao %d = %d\n", aux, soma);
   return soma;
 }
 
 int Vetor_Soma_Descendo (int v[], int n, int Indice) {
   int aux, soma;
-  for ( aux = soma = 0 ; aux < n-Indice-1 ; aux++ ) {
+  for ( aux = Indice+1, soma = 0 ; aux < n-Indice ; aux++ ) {
     soma += v[aux];
   }
+  printf("Soma Descendo na posicao %d = %d\n", aux, soma);
   return soma;
 }
 
@@ -42,11 +45,15 @@ int Indice_Menor_Elemento (int v[], int n) {
 }
 
 void Vetor_Ponto_de_Equilibrio (int v[], int n) {
-  int aux, v_aux[n-1];
+  int aux, v_aux[n-1], aux_ponto_equilibrio;
   for ( aux = 0 ; aux < n-1 ; aux ++) {
-    v_aux[aux] = abs(Vetor_Soma_Subindo (v,n,aux) - Vetor_Soma_Descendo (v,n,aux));
+    aux_ponto_equilibrio = ((Vetor_Soma_Subindo (v,n,aux) - Vetor_Soma_Descendo (v,n,aux))<0)?
+    ((Vetor_Soma_Subindo (v,n,aux) - Vetor_Soma_Descendo (v,n,aux))*(-1)):
+    (Vetor_Soma_Subindo (v,n,aux) - Vetor_Soma_Descendo (v,n,aux));
+    printf("Diferenca na posicao %d = %d\n", aux, aux_ponto_equilibrio);
+    v_aux[aux] = aux_ponto_equilibrio;
   }
-  printf("%d\n", Indice_Menor_Elemento(v_aux,n-1));
+  printf("%d\n", Indice_Menor_Elemento(v_aux,n));
 }
 
 int main () {

13e8bdc157a310e978f467262a99a0eb7b04d0ff
Gustavo G. Queiroz <43866024+GGDestruidor@users.noreply.github.com>
2019-08-13 09:48:48 -0300

    Add files via upload

---
+++ Computacao/vetor.h
@@ -0,0 +1,11 @@
+void Set_Vetor  (int v[], int t) {
+    for (int aux_cont = 0; aux_cont < t ; aux_cont++) {
+        scanf ("%d", &v[aux_cont]);
+    }
+}
+
+void Show_Vetor (int v[], int t) {
+    for (int aux_cont = 0; aux_cont < t ; aux_cont++) {
+        printf ("%d\n", v[aux_cont]);
+    }
+}

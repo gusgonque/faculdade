97db3c086f32a5ac4be92731087e3b0a81516721
Gustavo <gustavoguguqueiroz@hotmail.com>
2019-09-17 00:24:02 -0300

    17/09/2019 0.1

---
+++ Computacao/URI/C/1185_Unfinished.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void Preencher_Matriz (float mat[][20],int l,int c) {
+  int i, j;
+  for ( i = 0 ; i < l ; i++ )
+    for ( j = 0 ; j < c ; j++ )
+      scanf("%f", &mat[i][j]);
+}
+
+float Matriz_Soma_Abaixo_Diagonal_Secundaria  (float m[][20], int l, int c) {
+    int i,j;
+    float soma;
+    for ( i = soma = 0 ; i < l ; i++ )
+      for ( j = c-i-1 ; j > 0 ; j-- )
+        soma += m[i][j];
+    return soma;
+}
+
+float Matriz_Media_Abaixo_Diagonal_Secundaria (float m[][20], int l, int c) {
+    int i,j;
+    float media;
+    for ( i = media = 0 ; i < l ; i++ )
+      for ( j = c-i-1 ; j > 0 ; j-- )
+        media += m[i][j];
+    return (media/66);
+}
+
+int main () {
+    float m[20][20];
+    char c;
+    scanf("%c", &c);
+    Preencher_Matriz (m,12,12);
+    if (c == 'S')
+        printf("%.1f\n", Matriz_Soma_Abaixo_Diagonal_Secundaria (m,12,12));
+    if (c == 'M')
+        printf("%.1f\n", Matriz_Media_Abaixo_Diagonal_Secundaria (m,12,12));
+    return 0;
+}

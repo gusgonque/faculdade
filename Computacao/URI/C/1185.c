a659c78c30801cb4810fd778a200f152aad445db
Gustavo Queiroz <gustavoguguqueiroz@hotmail.com>
2019-09-23 15:18:06 -0300

    23-09-2019 1

--- Computacao/URI/C/1185.c
+++ Computacao/URI/C/1185.c
@@ -7,20 +7,20 @@ void Preencher_Matriz (float mat[][20],int l,int c) {
       scanf("%f", &mat[i][j]);
 }
 
-float Matriz_Soma_Abaixo_Diagonal_Secundaria  (float m[][20], int l, int c) {
+float Matriz_Soma_Acima_Diagonal_Secundaria  (float m[][20], int l, int c) {
     int i,j;
     float soma;
-    for ( i = soma = 0 ; i < l ; i++ )
-      for ( j = c-i-1 ; j > 0 ; j-- )
+    for ( i = soma = 0 ; i < l-1 ; i++ )
+      for ( j = 0 ; j < c-i-1 ; j++ )
         soma += m[i][j];
     return soma;
 }
 
-float Matriz_Media_Abaixo_Diagonal_Secundaria (float m[][20], int l, int c) {
+float Matriz_Media_Acima_Diagonal_Secundaria (float m[][20], int l, int c) {
     int i,j;
     float media;
-    for ( i = media = 0 ; i < l ; i++ )
-      for ( j = c-i-1 ; j > 0 ; j-- )
+    for ( i = media = 0 ; i < l-1 ; i++ )
+      for ( j = 0 ; j < c-i-1 ; j++ )
         media += m[i][j];
     return (media/66);
 }
@@ -31,8 +31,8 @@ int main () {
     scanf("%c", &c);
     Preencher_Matriz (m,12,12);
     if (c == 'S')
-        printf("%.1f\n", Matriz_Soma_Abaixo_Diagonal_Secundaria (m,12,12));
+        printf("%.1f\n", Matriz_Soma_Acima_Diagonal_Secundaria (m,12,12));
     if (c == 'M')
-        printf("%.1f\n", Matriz_Media_Abaixo_Diagonal_Secundaria (m,12,12));
+        printf("%.1f\n", Matriz_Media_Acima_Diagonal_Secundaria (m,12,12));
     return 0;
 }
---
+++ Computacao/URI/C/1186.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void Preencher_Matriz (float mat[][20],int l,int c) {
+  int i, j;
+  for ( i = 0 ; i < l ; i++ )
+    for ( j = 0 ; j < c ; j++ )
+      scanf("%f", &mat[i][j]);
+}
+
+float Matriz_Soma_Abaixo_Diagonal_Secundaria  (float m[][20], int l, int c) {
+    int i,j;
+    float soma;
+    for ( i = l-1, soma = 0 ; i > 0 ; i-- )
+      for ( j = c-1 ; j >= i ; j++ )
+        soma += m[i][j];
+    return soma;
+}
+
+float Matriz_Media_Abaixo_Diagonal_Secundaria (float m[][20], int l, int c) {
+    int i,j;
+    float media;
+    for ( i = l-1, media = 0 ; i > 0 ; i-- )
+      for ( j = c-1 ; j >= i ; j++ )
+        media += m[i][j];
+    return (media/66);
+}
+
+int main () {
+    float m[20][20];
+    char c;
+    scanf("%c", &c);
+    Preencher_Matriz (m,12,12);
+    if (c == 'S')
+        printf("%.1f\n", Matriz_Soma_Abaixo_Diagonal_Secundaria (m,12,12));
+    if (c == 'M')
+        printf("%.1f\n", Matriz_Media_Abaixo_Diagonal_Secundaria (m,12,12));
+    return 0;
+}

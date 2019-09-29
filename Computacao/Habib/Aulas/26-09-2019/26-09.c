a57f9e6cf86f299469eebd3965e1baf907d28e1d
Gustavo <gustavoguguqueiroz@hotmail.com>
2019-09-28 21:48:59 -0300

    28-09-2019 0.1

--- Computacao/Habib/Aulas/26-09-2019/26-09.c
+++ Computacao/Habib/Aulas/26-09-2019/26-09.c
@@ -8,23 +8,29 @@
 
 void Preencher_Matriz (float mat[][maxb],int *l,int *c) {
   int i, j;
+  printf("Teste\n");
   scanf("%d %d", l, c);
   for ( i = 0 ; i < *l ; i++ )
     for ( j = 0 ; j < *c ; j++ )
       scanf("%f%*c", &mat[i][j]);
 }
 
-void Ler_Nomes_Notas (char m_c[][maxl], float m_n[][maxb], int *nl, int nc) {
+void Ler_Nomes_Notas (char m_c[][maxc], float m_n[][maxb], int *nl, int nc) {
   int i, j;
   char s[maxc];
-  scanf("%[^;]%*c", s);
+  printf("TESTE 2\n");
+  scanf("%*c%[^;]%*c", &s);
+  printf("TESTE 3\n");
   for ( i=0 ; strcmp ( s , "fim" ) ; i++ ) {
     strcpy (m_c[i],s);
-    for ( j=0 ; j<nc ; j++ )
+    printf("TESTE 5\n");
+    for ( j=0 ; j<nc ; j++ ) {
       scanf("%f", &m_n[i][j]);
-    scanf("%*c");
-    scanf("%[^;]%*c", s);
+    }
+    printf("TESTE 6. strcmp ( %s , fim ) = %d\n", s, strcmp ( s , "fim" ));
+    scanf("%*c%[^;]%*c", &s);
   }
+  printf("TESTE 7\n");
   *nl = i;
 }
 
@@ -35,7 +41,7 @@ float Somaprod (float notas[],float pesos[],int cp) {
   return soma;
 }
 
-void Media_Pesos (float pesos[][maxb],int lp,int cp,float notas[][maxb],int ln, float medias[][maxb]) {
+void Calc_Medias_Pesos (float pesos[][maxb],int lp,int cp,float notas[][maxb],int ln, float medias[][maxb]) {
   int i, j;
   for ( i=0 ; i<ln ; i++ )
     for ( j=0 ; j<cp ; j++)
@@ -52,17 +58,19 @@ void Calc_Media_Bim (float notas[][maxb],int ln,int cp,float med_bim[]) {
   }
 }
 
-void Leitura (float m_pes[][maxb],int *lp,int *cp,float m_nom[][maxc],float m_not[][maxb],int *ln,int *cn) {
-  Preencher_Matriz (m_pes,lp,lc);
-  Ler_Nomes_Notas (m_nom,m_not,ln,*lc);
+void Leitura (float m_pes[][maxb],int *lp,int *cp,char m_nom[][maxc],float m_not[][maxb],int *ln) {
+  printf("Preencher a matriz dos pesos\n");
+  Preencher_Matriz (m_pes,lp,cp);
+  printf("Preencher a matriz dos nomes\n");
+  Ler_Nomes_Notas (m_nom,m_not,ln,*cp);
 }
 
-void Calculo (float m_pes[][maxb],int lp,int cp,float m_nom[][maxc],float m_not[][maxb],int ln, float m_med[][maxb],float v_medbim[]) {
+void Calculo (float m_pes[][maxb],int lp,int cp,char m_nom[][maxc],float m_not[][maxb],int ln, float m_med[][maxb],float v_medbim[]) {
   Calc_Medias_Pesos (m_pes,lp,cp,m_not,ln,m_med);
   Calc_Media_Bim (m_not,ln,cp,v_medbim);
 }
 
-void Relatorio (char nomes[][maxl],int ln,float notas[][maxb],int cn,float medias[][maxb],int cm,float med_bim[]) {
+void Relatorio (char nomes[][maxc],int ln,float notas[][maxb],int cn,float medias[][maxb],int cm,float med_bim[]) {
   int i,j;
   printf("%-20s","   NOME");
   for ( i=1 ; i<=cn ; i++)
@@ -88,11 +96,11 @@ int main () {
   int lp,cp,ln,cn;
   float m_pes[maxl][maxb],m_not[maxl][maxb],m_med[maxl][maxb],v_medbim[maxb];
   char m_nom[maxl][maxc];
-
-  Leitura (m_pes,&lp,&cp,m_nom,m_not,&ln,&cn);
-
+  printf("Leitura de dados\n");
+  Leitura (m_pes,&lp,&cp,m_nom,m_not,&ln);
+  printf("Calculando dados\n");
   Calculo (m_pes,lp,cn,m_nom,m_not,ln,m_med,v_medbim);
-
+  printf("Apresentando relatorio\n");
   Relatorio (m_nom,ln,m_not,cn,m_med,lp,v_medbim);
 
   return 0;

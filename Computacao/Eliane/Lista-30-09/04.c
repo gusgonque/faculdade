a983425cb563459edddd25a48586ee6874813a5b
Gustavo Queiroz <gustavoguguqueiroz@hotmail.com>
2019-10-01 08:34:42 -0300

    01-10-2019 0.1

--- Computacao/Eliane/Lista-30-09/04.c
+++ Computacao/Eliane/Lista-30-09/04.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 
-void Preencher_Matriz (int m[][70], int nl, int nc, int x) {
-  int i, j;
-  for ( i=0 ; i<nl ; i++ )
-    for ( j=0 ; j<nc ; j++ )
-      m[i][j]=x;
-}
-
-void Preencher_Diagonal_Secundaria_Matriz(int m[][70], int nl, int nc, int x) {
-  int i, j;
-  for ( i=0, j=nc-1 ; i<nl ; i++,j-- )
-      m[i][j]=x;
-}
-
-void Preencher_Diagonal_Principal_Matriz(int m[][70], int nl, int nc, int x) {
-  int i;
-  for ( i=0 ; i<nl ; i++ )
-      m[i][i] = x;
-}
-
-void Processo (int m[][70], int n) {
-  Preencher_Matriz(m, n, n, 3);
-  Preencher_Diagonal_Principal_Matriz(m, n, n, 1);
-  Preencher_Diagonal_Secundaria_Matriz(m, n, n, 2);
+void Preencher_Matriz (int m[][70], int nl, int nc) {
+  int i, j, k;
+  for ( i=0, k=nc-1 ; i<nl ; i++, k-- ){
+    for ( j=0 ; j<nc ; j++ ){
+      m[i][j]= 3;
+    }
+    m[i][i] = 1;
+    m[i][k] = 2;
+  }
 }
 
 void Relatorio (int m[][70], int nl, int nc) {
@@ -38,7 +24,7 @@ int main () {
   int m[70][70], n;
   scanf("%d", &n);
   for (  ; n >= 3 ; scanf("%d", &n) ) {
-    Processo(m,n);
+    Preencher_Matriz(m, n, n);
     Relatorio(m, n, n);
   }
   return 0;

67c041c9f7b1193fce18f0e9658e52c7fbc2b4b9
Gustavo <gustavoguguqueiroz@hotmail.com>
2019-12-11 21:00:49 -0300

    11-12-2019 2

--- Computacao/Habib/02_PALAVRAS/02.c
+++ Computacao/Habib/02_PALAVRAS/02.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #define MAX 1000
 #define MAXC 50
 
 void Leitura ( char m_pal[][MAXC], int *t , char *fn )
 {
+
   int i;
   FILE *arq;
 
@@ -15,7 +18,35 @@ void Leitura ( char m_pal[][MAXC], int *t , char *fn )
   fclose (arq);
 
 }
+//lindo me salva pfvr
 
+void Organizar_Ordem_Alfabetica ( char m_pal[][MAXC] , int t )
+{
+  int i, j, k;
+  char aux[MAXC];
+
+  for ( i=0 ; i<t ; i++ )
+
+    for ( j=0 ; j < strlen ( m_pal[i] ) ; j++ )
+
+      if ( isalpha ( m_pal[i][j] ) )
+      {
+        if (j != 0)
+        {
+
+          for ( k = 0 ; k < strlen ( m_pal[i] )-j ; k++ )
+            aux [k] = m_pal [i][j+k];
+
+
+        }
+
+      }
+}
+
+void Processo ( )
+{
+
+}
 
 int main ()
 {

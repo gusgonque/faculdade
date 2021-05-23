f408a3a7c6e49b65f15a31114acac788a7beda60
Gustavo <gustavoguguqueiroz@hotmail.com>
2021-05-23 00:53:04 -0300

    23/5

--- AED/T2/Programa.c
+++ AED/T2/Programa.c
@@ -5,11 +5,15 @@
     quantidade em estoque: número inteiro
     preço unitário: número real com 2 casas decimais
     localização: string contendo a descrição do local em que o produto se encontra armazenado (100 caracteres no máximo)
+
     Todas as informaçõs para o operação de inserção são obrigatórias.
+
  - Remover: dado o código de um produto, remove-o do cadastro
  - Alterar Estoque: altera a quantidade em estoque
  - Alterar Preço: altera o preço unitário
  - Alterar Localização: altera a localização do produto
+
+
  - Carregar Arquivo texto: faz a carga de um arquivo texto, cujo caminho é informado pelo usuário, contendo operações e respectivas informações a serem aplicadas ao cadastro
  - Informações do produto: a partir do código de um produto específico, imprime suas informações
  - Listar produtos: lista as informações dos produtos do cadastro por ordem crescente de código
@@ -18,36 +22,231 @@
 
 
 I;20;parafuso 3mm;500;2.00;prateleira 5A
-%c%*c%[^;]d%*c%[^;]s%*c%[^;]d%*c%[^;]f%*c%[^;]s
+%c%*c
+%d%*c%[^;]%*c%d%*c%f%*c%[^\n]
+
 
 */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct no
 {
   int cod;
+  int qua;
+  float pre;
+  char nom[50], loc[100];
+
   struct no * esq;
   struct no * dir;
 };
+typedef struct no* arvPro;
 
-typedef struct no* arvore;
+void criar_arvore (arvPro *r)
+{
+    *r = NULL;
+}
 
-int vazio (arvore r) {
-return (r == NULL);
+int vazia (arvPro r)
+{
+  return (r == NULL);
+}
+
+arvPro busca_arvore_binaria (arvPro r, int x)
+{
+  if(vazia(r))
+    return NULL;
+  if(r->cod > x)
+    return busca_arvore_binaria(r->esq, x);
+  if(r->cod < x)
+    return busca_arvore_binaria(r->dir, x);
+  return r;
+}
+
+// Pr´e-condi¸c~ao: ´arvore n~ao vazia
+int maximo(arvPro r)
+{
+  while(r->dir != NULL)
+    r = r->dir;
+  return r->cod;
+}
+
+// Pr´e-condi¸c~ao: ´arvore n~ao vazia
+int minimo(arvPro r)
+{
+  while(r->esq != NULL)
+    r = r->esq;
+  return r->cod;
+}
+
+arvPro remove_arvore_binaria (arvPro r, int x)
+{
+  if(vazia(r))
+    return NULL;
+    if(x < r->cod)
+      r->esq = remove_arvore_binaria(r->esq, x);
+    else if(x > r->cod)
+      r->dir = remove_arvore_binaria(r->dir, x);
+    else // x == r->cod
+    if(r->esq == NULL && r->dir == NULL)
+    { // ´e n´o folha
+      free(r);
+      r = NULL;
+    }
+    else if(r->esq == NULL)
+    { // s´o tem filho da direita
+      r->cod = minimo(r->dir);
+      r->dir = remove_arvore_binaria(r->dir,r->cod);
+    }
+    else
+    { // tem 2 filhos ou s´o o da esquerda
+      r->cod = maximo(r->esq);
+      r->esq = remove_arvore_binaria(r->esq,r->cod);
+    }
+    return r;
+}
+
+arvPro insere_arvore_binaria(arvPro r, int x)
+{
+
+  if(vazia(r))
+  {
+    r = (struct no*) malloc(sizeof(struct no));
+    r->cod = x;
+    r->esq = NULL;
+    r->dir = NULL;
+  }
+  else
+  if(x < r->cod)
+    r->esq = insere_arvore_binaria(r->esq, x);
+  else // x >= r->cod
+    r->dir = insere_arvore_binaria(r->dir, x);
+  return r;
+}
+
+int Inserir_Produto(arvPro prod)
+{
+  int x;
+
+  printf(" Para inserir um novo produto, digite o numero do codigo dele.\n - ");
+  scanf("%d", &x);
+
+  printf("vazia = %d\n",vazia(busca_arvore_binaria (prod,x)) );
+
+  while (vazia(busca_arvore_binaria (prod,x)) == 0)
+  {
+    printf(" Codigo ja utilizado por outro produto ou invalido, digite -1 para retornar ao Menu, ou digite outro codigo.\n - ");
+    scanf("%d", &x);
+    if (x == -1)
+      return 1;
+  }
+
+  prod = insere_arvore_binaria(prod, x);
+  printf(" prod->cod = %d\n", prod->cod);
+
+  /*
+  prod->qua;
+  prod->pre;
+  prod->nom;
+  prod->loc;
+  */
+
+  return 1;
+}
+
+void Remover_Produto(){
+
+}
+void Alterar_Produto(){
+
+}
+void Carregar_Arquivo(){
+
+}
+void Info_Produto(){
+
+}
+void Listar_Produtos(){
+
+}
+void Imprimir(){
+
+}
+
+int Menu (arvPro prod)
+{
+  int x;
+
+  printf("\n Menu Principal\n\n Digite a opcao desejada:\n");
+  printf(" 1 - Inserir um produto.\n");
+  printf(" 2 - Remover um produto.\n");
+  printf(" 3 - Menu de alteracao de produtos.\n");
+  printf(" 4 - Carregar arquivo texto.\n");
+  printf(" 5 - Informacoes do produto.\n");
+  printf(" 6 - Listar produtos.\n");
+  printf(" 7 - Menu de impressao.\n");
+  printf(" 8 - Encerrar programa.\n");
+  printf(" - ");
+  scanf("%d",&x);
+
+  while (x>8 || x<1)
+  {
+    printf("Comando invalido. Digite novamente.\n -");
+    scanf("%d",&x);
+  }
+
+  switch (x)
+  {
+    case 1:
+    {
+      return Inserir_Produto(prod);
+    }
+    case 2:
+    {
+      Remover_Produto();
+      return 1;
+    }
+    case 3:
+    {
+      Alterar_Produto();
+      return 1;
+    }
+    case 4:
+    {
+      Carregar_Arquivo();
+      return 1;
+    }
+    case 5:
+    {
+      Info_Produto();
+      return 1;
+    }
+    case 6:
+    {
+      Listar_Produtos();
+      return 1;
+    }
+    case 7:
+    {
+      Imprimir();
+      return 1;
+    }
+    case 8:
+    {
+      free(prod);
+      return 0;
+    }
+  }
 }
 
 int main()
 {
-  char ope, nom[50], loc[100], linha[200];
-  int cod, qua;
-  float pre;
+  arvPro produtos;
+  criar_arvore (&produtos);
 
-  scanf("%c%*c", &ope);
+  while (Menu(produtos));
 
-  if (ope == 'I') {
-    scanf("%d%*c%[^;]%*c%d%*c%f%*c%[^\n]", &cod, &nom, &qua, &pre, &loc);
-  }
-  printf("%c;%d;%s;%d;%.2f;%s", ope, cod, nom, qua, pre, loc);
   return 0;
 }

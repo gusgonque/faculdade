3b39154e94c73da13650b68d680f21b193e7eef4
Gustavo <gustavoguguqueiroz@hotmail.com>
2022-03-10 15:40:17 -0300

    08 03 2022 aed+1

---
+++ AED/Listas/Lista1/7.c
@@ -0,0 +1,22 @@
+// 7. Projete um algoritmo recursivo que dada uma cadeia de caracteres str, decida se str ´e pal´ındrome ou n˜ao.
+#include <stdio.h>
+#include <string.h>
+
+int palindromo (char *i, char *f){ // i=inicio  f=final (em relação à string)
+    if (i==f)
+        return 1;
+    if (*i == *f){
+        return 1 && palindromo (i+1,f-1);
+    }
+    else
+        return 0;
+}
+
+int main (){
+    char str [5] = "arara";
+    if (palindromo(str,str+strlen(str)-1))
+        printf("Eh palindromo.\n");
+    else
+        printf("Nao eh palindromo.\n");
+    return 0;
+}

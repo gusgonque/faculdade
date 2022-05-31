1b184cc774960f7a759c6ec468a47d70d36634db
Gustavo <gustavoguguqueiroz@hotmail.com>
2022-05-30 21:15:26 -0300

    changes

--- AED/Trab2/trab2.c
+++ AED/Trab2/trab2.c
@@ -2,8 +2,117 @@
 #include <stdio.h>
 
 void interfacePrincipal(){
-    int x;
-    printf(" Bem vindo a interface principal!\n Digite um numero para continuar.\n");
+    int x, i;
+    printf("Bem vindo a interface principal!\nDigite um numero para continuar.\n");
+    printf("1 - Interface de registro de usuario. (Inserir/Alterar/Remover)\n");
+    printf("2 - Carregar arquivo texto.\n");
+    printf("3 - Consultar profissional.\n");
+    printf("4 - Listar profissionais.\n");
+    printf("5 - Interface da arvore.\n");
+    printf("0 - Finalizar programa.\n");
+
+    printf(">");
     scanf("%d",&x);
-    printf("x = %d",x);
+    while (x<0 || x>5){
+        printf("Comando invalido. Digite novamente.\n");
+        printf(">");
+        scanf("%d",&x);
+    }
+
+    switch (x) {
+        case 1:
+            if (!interfaceRegistro())
+                interfacePrincipal();
+        case 2:
+
+        case 3:
+
+        case 4:
+
+        case 5:
+
+        case 0: ;
+
+    }
+}
+
+int interfaceRegistro(){
+    int x;
+    printf("Bem vindo a interface de registro de profissionais!\nDigite um numero para continuar.\n");
+    printf("1 - Inserir novo profissional.\n");
+    printf("2 - Alterar endereco de um profissional.\n");
+    printf("3 - Alterar telefone de um profissional.\n");
+    printf("4 - Remover um profissional.\n");
+    printf("0 - retornar ao menu anterior.\n");
+
+    printf(">");
+    scanf("%d",&x);
+    while (x<0 || x>4){
+        printf("Comando invalido. Digite novamente.\n");
+        printf(">");
+        scanf("%d",&x);
+    }
+
+    switch (x) {
+        case 1:
+            inserirProfissionalManual();
+            return 0;
+        case 2:
+
+        case 3:
+
+        case 4:
+
+        case 5:
+
+        case 0:
+            return 0;
+    }
+    return 1;
+}
+
+int verificarNumero(long long int x){
+
+}
+
+int verificarSting(char s[]){
+
+}
+
+void inserirProfissionalManual(){
+    profissional novPro;
+    printf("Digite o codigo do profissional a ser inserido.\n>");
+    scanf("%d",&novPro.cod);
+    if(!verificarNumero(novPro.cod)){
+        printf("Nao e possivel usar esse codigo. Tente novamente.\n>");
+        scanf("%d", &novPro.cod);
+    }
+
+    printf("Digite o nome do profissional a ser inserido.\n>");
+    scanf("%s", &novPro.nom);
+
+    printf("Digite o CPF do profissional a ser inserido.\n>");
+    scanf("%d", &novPro.cpf);
+    if(!verificarNumero(novPro.cpf)){
+        printf("Nao e possivel usar esse CPF. Tente novamente.\n>");
+        scanf("%d", &novPro.cpf);
+    }
+
+    printf("Digite o numero do registro profissional a ser inserido.\n>");
+    scanf("%s", &novPro.numReg);
+    if(!verificarSting(novPro.numReg)){
+        printf("Nao e possivel usar esse numero. Tente novamente.\n>");
+        scanf("%d", &novPro.numReg);
+    }
+
+    printf("Digite o nome do profissional a ser inserido.\n>");
+    scanf("%s", &novPro.nom);
+
+    printf("Digite o endereco do profissional a ser inserido.\n>");
+    scanf("%s", &novPro.end);
+    if(!verificarSting(novPro.end)){
+        printf("Nao e possivel usar esse CPF. Tente novamente.\n>");
+        scanf("%s", &novPro.end);
+    }
+
 }
--- AED/Trab2/trab2.h
+++ AED/Trab2/trab2.h
@@ -1,6 +1,28 @@
 #ifndef FACULDADE_TRAB2_H_INCLUDED
 #define FACULDADE_TRAB2_H_INCLUDED
 
-void interfacePrincipal();
+typedef struct{
+    int cod;
+    char nom[50];
+    long long int cpf;
+    char numReg[30];
+    char end[100];
+    long long int tel;
+} profissional;
 
-#endif //FACULDADE_TRAB2_H
+#define TAM 4
+
+typedef struct{
+    int cha[TAM];
+    int ptDad[TAM];
+    int fil[TAM+1];
+} registro;
+
+void interfacePrincipal();
+int interfaceRegistro();
+
+void inserirProfissionalManual();
+void inserirProfissionalArquivo();
+void inserirProfissional();
+
+#endif //FACULDADE_TRAB2_H_INCLUDED
